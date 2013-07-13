2f28874d900e6ed01b76aad44138240c57a8a0bc
jiangfengming850811 <53422245@qq.com>
2013-07-13 16:58:48 +0800

    修改部分

---
+++ ROOT/SimpTest/src/main/test/com/yada/eom/weixin/impl/GetBillDetailsTest.java
@@ -0,0 +1,19 @@
+package com.yada.eom.weixin.impl;
+
+import org.junit.Test;
+
+import com.yada.eom.core.SynchEOMClient;
+import com.yada.eom.order.impl.GQ045004_GR045004;
+
+public class GetBillDetailsTest {
+
+	@Test
+	public void testGet() {
+		String path = GetAnnualFeeWaiverTest.class.getClassLoader().getResource("").getPath();
+		path = path.replaceAll("test-classes", "classes");
+		SynchEOMClient eomClient = new SynchEOMClient(path);
+		GQ045004_GR045004 cq008003 = new GQ045004_GR045004(eomClient);
+		GetBillDetails getBillDetails = new GetBillDetails("1", cq008003);
+		getBillDetails.get("001A03F70055D4C8","4");
+	}
+}
